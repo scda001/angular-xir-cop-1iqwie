393d8feb67975b2f74b4b64b54625e78f3d6e45c
scda001 <daniel.schweizer@sylabs.ch>
2020-04-27 14:59:22 +0200

    5 files modified, 4 added, 3 deleted; BP autocomplete with street example

---
+++ src/app/business-partner-info/business-partner-info.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {startWith, map} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-business-partner-info',
+  templateUrl: './business-partner-info.component.html',
+  styleUrls: ['./business-partner-info.component.css']
+})
+export class BusinessPartnerInfoComponent implements OnInit {
+  control = new FormControl();
+  streets: string[] = ['Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue'];
+  filteredStreets: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredStreets = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    return this.streets.filter(street => this._normalizeValue(street).includes(filterValue));
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
+}
